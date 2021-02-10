22a0108111e5f35891e17bd82610462facf728d7
Philip Bollen <trik-flip@users.noreply.github.com>
2021-02-10 16:49:50 +0100

    Add Unittesting

---
+++ tests/core_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MainTestCases(unittest.TestCase):
+    def test_complete(self):
+        """This test will always complete"""
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
